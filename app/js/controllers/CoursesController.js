f3225a31e201c0921fd923e78949145b62e631a8
alexwander <ryndyuk.alexander@gmail.com>
2016-06-10 20:22:45 +0300

    add controller

---
+++ app/js/controllers/CoursesController.js
@@ -0,0 +1,22 @@
+/**
+ * Created by alexandr on 05.06.2016.
+ */
+'use strict';
+
+coursesApp.controller('CoursesController',
+    function CoursesController($scope) {
+        $scope.event = {
+
+            name: 'Angular f messsage',
+            date: '1/1/1990',
+            time: '10:30 am',
+
+
+
+            imageUrl: 'img/angularjs-logo.png'
+        }
+
+
+
+    }
+);
