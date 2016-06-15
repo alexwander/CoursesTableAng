83f670164c8f25c43f121ee0187698561db7609a
alexwander <ryndyuk.alexander@gmail.com>
2016-06-15 23:50:03 +0300

    new elements

--- app/js/controllers/CoursesController.js
+++ app/js/controllers/CoursesController.js
@@ -4,37 +4,8 @@
 'use strict';
 
 coursesApp.controller('CoursesController',
-    function CoursesController($scope) {
-        $scope.event = {
-
-            name: 'Angular f messsage',
-            date: '1/1/1990',
-            time: '10:30 am',
-            imageUrl: 'img/angularjs-logo.png',
-            courses: [
-                {
-                    name: 'Core Computer Science',
-                    credits: 'These courses make of the building blocks of the computer science degree.',
-                    selection: 'null',
-                    logo: 'img/logo.png'
-
-                },
-
-                {
-                    name: 'Upper Level Computer Science',
-                    credits: 'These computer science degree courses make of the building blocks of the .',
-                    selection: 'null',
-                    logo: 'img/logo.png'
-                },
-
-                {
-                    name: 'Math',
-                    credits: 'YUour science degree courses make of the building blocks of the computer .',
-                    selection: 'null',
-                    logo: 'img/logo.png'
-                }],
-
-        }
+    function CoursesController($scope, courseData) {
+        $scope.event = courseData.course;
 
 
         $scope.checkAll = function () {
--- app/js/services/CourseData.js
+++ app/js/services/CourseData.js
@@ -2,3 +2,39 @@
  * Created by alexandr on 15.06.2016.
  */
 
+coursesApp.factory('courseData', function () {
+    return {
+        course: {
+            name: 'Angular f messsage',
+            date: '1/1/1990',
+            time: '10:30 am',
+            imageUrl: 'img/angularjs-logo.png',
+            courses: [
+                {
+                    name: 'Core Computer Science',
+                    credits: 'These courses make of the building blocks of the computer science degree.',
+                    selection: 'null',
+                    logo: 'img/logo.png'
+
+                },
+
+                {
+                    name: 'Upper Level Computer Science',
+                    credits: 'These computer science degree courses make of the building blocks of the .',
+                    selection: 'null',
+                    logo: 'img/logo.png'
+                },
+
+                {
+                    name: 'Math',
+                    credits: 'YUour science degree courses make of the building blocks of the computer .',
+                    selection: 'null',
+                    logo: 'img/logo.png'
+                }],
+
+        }
+
+
+    };
+
+});
