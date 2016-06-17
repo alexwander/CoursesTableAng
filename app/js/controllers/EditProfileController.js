770deb3a285a570797286d68e4a19ba1d1e05766
alexwander <ryndyuk.alexander@gmail.com>
2016-06-17 18:38:11 +0300

    new elements

--- app/js/controllers/EditProfileController.js
+++ app/js/controllers/EditProfileController.js
@@ -1,3 +1,20 @@
 /**
  * Created by alexandr on 15.06.2016.
  */
+
+coursesApp.controller('EditProfileController',
+    function EditProfileController($scope) {
+        $scope.user = [];
+
+
+
+
+    }
+
+
+
+
+
+
+
+);
